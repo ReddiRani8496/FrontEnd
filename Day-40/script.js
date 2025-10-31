eded46ca223da0f7a7090ff721ef06eb493e1fa3
reddirani8496 <reddiranibattuluri@gmail.com>
2025-10-31 22:41:47 +0530

    front end code uploaded

---
+++ Day-40/script.js
@@ -0,0 +1,48 @@
+// let a = 10;
+// // console.log("a value is ", a);
+
+// let b = 20;
+
+// if (a > b) {
+//   console.log("a is bigger than b");
+// } else {
+//   console.log(b, " is bigger than ", a);
+// }
+
+// let a = 10;
+// let b = "hello";
+
+// console.log(a, "rock", a); // a = 10   rock 10
+
+// let a = 1;
+// let b = 3;
+// console.log(a % b);
+
+// let a = 10;
+// let b = 20;
+
+// let c = a + b;
+// console.log(c);
+
+// c %= 20; // 30 + 20
+// console.log(c);
+
+// == >= > < <= != !== ===
+
+let a = 10;
+
+let b = "10";
+
+console.log(a == b);
+
+// == it will  check only the value
+
+// 10==10
+
+// if we want to check both type and value then use ===
+
+console.log(a === b);
+
+console.log(a != b);
+
+console.log(a !== b); // it will check both the type and value
