f7c943aa1026c489a4c31a89f4d3722405296c3c
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-01 19:36:58 +0530

    operators uploaded

--- Day-40/script.js
+++ Day-40/script.js
@@ -29,20 +29,52 @@
 
 // == >= > < <= != !== ===
 
-let a = 10;
+// let a = 10;
 
-let b = "10";
+// let b = "10";
 
-console.log(a == b);
+// console.log(a == b);
 
-// == it will  check only the value
+// // == it will  check only the value
 
-// 10==10
+// // 10==10
 
-// if we want to check both type and value then use ===
+// // if we want to check both type and value then use ===
 
-console.log(a === b);
+// console.log(a === b);
 
-console.log(a != b);
+// console.log(a != b);
 
-console.log(a !== b); // it will check both the type and value
+// console.log(a !== b); // it will check both the type and value
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a ** b); // 10 * 5 // 1,00,000, a power b // 10 ^ 5 => 1,00,000
+
+// let a = 10;
+// let b = 10;
+// console.log(a !== b);
+
+// unary operators
+// ++ -- => pre incr / post incr  pre dec/post dec
+
+// let a = 10;
+// // console.log(a++); // 10 , later will increment by 1
+// // console.log(a); // 11
+
+// console.log(a--); // print 10, then dec
+// console.log(a);
+
+// logical operators
+// || &&
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a && b); // 10 && 20, if first value is true, then it will print second value
+// in javascript except 0 everything will be true, 0->false
+
+let a = true;
+let b = 0;
+console.log(b && a); // if first value is false, then it will return first value
