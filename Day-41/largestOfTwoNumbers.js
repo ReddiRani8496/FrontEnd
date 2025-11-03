7e3add4c5aab7fcaa714a92a772fb3ecc2b2bfcb
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-03 22:27:11 +0530

    conditional statements uploaded

---
+++ Day-41/largestOfTwoNumbers.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 5;
+if (a > b) {
+  console.log(a);
+} else if (b > a) {
+  console.log(b);
+} else {
+  console.log("both are equal");
+}
---
+++ Day-41/script.js
@@ -0,0 +1,28 @@
+let day = "lkjsdf";
+
+switch (day) {
+  case "monday":
+    console.log("Today is Monday");
+    break;
+
+  case "tuesday":
+    console.log("Today is Tuesday");
+    break;
+  case "wednesday":
+    console.log("Today is Wednesday");
+    break;
+  case "thursday":
+    console.log("Today is Thursday");
+    break;
+  case "friday":
+    console.log("Today is Friday");
+    break;
+  case "saturday":
+    console.log("Today is Saturday");
+    break;
+  case "sunday":
+    console.log("Today is Sunday");
+    break;
+  default:
+    console.log("Enter days between Sunday to Saturday");
+}
