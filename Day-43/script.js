6b5e85b76697411bd6531bf9bb9e6d74d7bd2d93
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-05 09:26:22 +0530

    loops programs uploaded

---
+++ Day-43/script.js
@@ -0,0 +1,33 @@
+// let val = true;
+// while (val) {
+//   console.log("hello");
+//   val = false;
+// }
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// find sum of digits in a number
+
+// let num = 178;
+
+// let sum = 0;
+
+// while (num != 0) {
+//   sum += num % 10;
+//   num = Math.floor(num / 10);
+// }
+
+// console.log("sum is ", sum);
+
+// do while loop
+
+let num = 5;
+
+do {
+  console.log(num);
+  num = num + 1;
+} while (num < 10);
