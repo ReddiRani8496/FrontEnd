6da5b3eeca669858e3d3a8492938e772c2706c3b
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-09 21:36:54 +0530

    function programs uploaded

---
+++ Day-47/script.js
@@ -0,0 +1,83 @@
+// function printHello() {
+//   console.log("Hello");
+//   let a = 10;
+
+//   console.log(a);
+
+//   //   var b = 20;
+//   //   console.log(b);
+// }
+
+// // console.log(b);
+
+// printHello();
+// // printHello();
+// // printHello();
+
+// // if (true) {
+// //   var num = 20;
+// // }
+// // console.log(num);
+
+// add two numbers
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// add(10, 20);
+
+// function increment(a) {
+//   console.log(a + 1);
+// }
+
+// increment(10);
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// add(10, 30);
+
+// function printString(str) {
+//   //   console.log(str.toUpperCase());
+//   return str.toUpperCase();
+// }
+
+// console.log(printString("Hello"));
+
+// count number of vowels in given string(a,e,i,o,u)
+
+// function countVowels(str) {
+//   str = str.toLowerCase();
+//   let count = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (
+//       str.charAt(i) == "a" ||
+//       str.charAt(i) == "e" ||
+//       str.charAt(i) == "i" ||
+//       str.charAt(i) == "o" ||
+//       str.charAt(i) == "u"
+//     ) {
+//       count++;
+//     }
+//   }
+
+//   console.log(count);
+// }
+// countVowels("hello rock star");
+
+// function addTwoNumbers(a,b)
+
+let addTwoNumbers = function (a, b) {
+  console.log(a + b);
+};
+
+addTwoNumbers(10, 20);
+
+let printHello = function () {
+  console.log("Hello");
+};
+
+printHello();
