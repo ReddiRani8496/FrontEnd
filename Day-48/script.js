89ada437db6380ae5f9710178f5f4de43d6f2bda
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-11 22:06:14 +0530

    DOM

---
+++ Day-48/script.js
@@ -0,0 +1,25 @@
+// console.log("hello");
+
+// console.log(window.document);
+
+// console.dir(window.document);
+
+// alert("Welcome to java script");
+
+// console.dir(window.document.body);
+
+// console.log(document.body.childNodes[5]);
+
+// document.body.childNodes[5]x.innerText = "On the Bulb";
+
+let firstButton = document.getElementById("firstbutton");
+// console.log(firstButton);
+
+firstButton.innerText = "On the Bulb";
+
+let secondButton = document.getElementsByClassName("secondbutton");
+
+console.log(secondButton);
+
+secondButton[0].innerText = "Off the Bulb";
+secondButton[1].innerText = "hello";
