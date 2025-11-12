d7c5eb92bc67cb214339567f8e1a9a944ea3e241
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-12 20:20:37 +0530

    Dom uploaded

--- Day-48/script.js
+++ Day-48/script.js
@@ -12,14 +12,45 @@
 
 // document.body.childNodes[5]x.innerText = "On the Bulb";
 
-let firstButton = document.getElementById("firstbutton");
-// console.log(firstButton);
+// let firstButton = document.getElementById("firstbutton");
+// // console.log(firstButton);
 
-firstButton.innerText = "On the Bulb";
+// firstButton.innerText = "On the Bulb";
 
-let secondButton = document.getElementsByClassName("secondbutton");
+// let secondButton = document.getElementsByClassName("secondbutton");
 
-console.log(secondButton);
+// console.log(secondButton);
 
-secondButton[0].innerText = "Off the Bulb";
-secondButton[1].innerText = "hello";
+// secondButton[0].innerText = "Off the Bulb";
+// secondButton[1].innerText = "hello";
+
+// let elements = document.getElementsByTagName("button");
+// console.log(elements);
+
+// console.log(elements[0].innerHTML);
+
+// elements[0].innerHTML = "<h1>Hello</h1>";
+
+// elements[1].innerText = "<h1>Hello</h1>";
+
+// let element = document.querySelector("button");
+// console.log(element);
+
+// let elements1 = document.querySelectorAll("button");
+// console.log(elements1);
+
+// let firstDiv = document.getElementById("div1");
+
+// firstDiv.innerText = "Div 1";
+
+// let secondDiv = document.getElementById("div2");
+
+// secondDiv.innerText = "Div 2";
+
+// let thirdDiv = document.getElementById("div3");
+
+// thirdDiv.innerText = "Div 3";
+
+let heading = document.querySelector("h1");
+
+heading.innerText = heading.innerText + " Good Morning!";
