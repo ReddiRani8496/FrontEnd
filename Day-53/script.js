3774589435407802990972c3a18f50dab0b6a0b5
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-25 22:13:01 +0530

    promise programs uploaded

--- Day-53/script.js
+++ Day-53/script.js
@@ -27,19 +27,64 @@
 //   console.log("hey");
 // }, 3000);
 
-function sum(a, b) {
-  console.log(a + b); // 30
+// function sum(a, b) {
+//   console.log(a + b); // 30
+// }
+
+// function sub(a, b) {
+//   console.log(a - b); // -10
+// }
+
+// function calculator(a, b, calcFunction) {
+//   // 10, 20,
+//   calcFunction(a, b);
+// }
+
+// calculator(10, 20, sum);
+
+// calculator(10, 20, sub);
+
+// console.log("before timeout");
+// function printData(data) {
+//   setTimeout(() => {
+//     console.log(data);
+//   }, 3000);
+// }
+
+// printData(10); // after 3 seconds 10
+// printData(20); // after print 10, then three seconds 20
+// printData(30); // after 3 seconds 30
+
+// console.log("line number 58");
+
+// function printData(data, nextPrintData) {
+//   setTimeout(() => {
+//     console.log(data); // 10, 20
+//     if (nextPrintData) {
+//       nextPrintData(); //printData(20)
+//     }
+//   }, 3000);
+// }
+
+// printData(10, () => {
+//   printData(20, () => {
+//     printData(30, () => {
+//       printData(40, () => {
+//         printData(50, () => {
+//           printData(60);
+//         });
+//       });
+//     });
+//   });
+// });
+
+function printData() {
+  return new Promise((resolve, reject) => {
+    console.log("inside promise");
+    reject("error ");
+  });
 }
 
-function sub(a, b) {
-  console.log(a - b); // -10
-}
+let data = printData();
 
-function calculator(a, b, calcFunction) {
-  // 10, 20,
-  calcFunction(a, b);
-}
-
-calculator(10, 20, sum);
-
-calculator(10, 20, sub);
+console.log(data);
