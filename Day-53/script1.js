8da7096e6ef5370582627680de9650fb7c002147
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-27 21:49:39 +0530

    api uploaded

--- Day-53/script1.js
+++ Day-53/script1.js
@@ -1,16 +1,16 @@
-function api() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("api data");
-      resolve("success");
-    }, 3000);
-  });
-}
+const url = "https://official-joke-api.appspot.com/random_joke";
 
-async function fetchData() {
-  for (let i = 0; i < 50; i++) {
-    await api();
-  }
-}
+// fetch(url) -> to make api call and get the data
 
-fetchData();
+// async and await
+
+let buttonElement = document.getElementById("button");
+let jokeElement = document.getElementById("joke");
+
+const getJoke = async () => {
+  const response = await fetch(url);
+  const data = await response.json(); // javascript object notation
+  jokeElement.innerText = data.setup;
+};
+
+buttonElement.addEventListener("click", getJoke);
