96410618529ab93b5d780928703e35f015756305
reddirani8496 <reddiranibattuluri@gmail.com>
2025-11-30 18:00:46 +0530

    Currency converter uploaded

---
+++ Day-55/codes.js
@@ -0,0 +1,13 @@
+// object -> key value
+// first one is currency code, 2nd is flag code
+
+const countryList = {
+  INR: "IN",
+  USD: "US",
+  EUR: "AD",
+  AED: "AE",
+  AFN: "AF",
+  BDT: "BD",
+  CNY: "CN",
+  LKR: "LK",
+};
