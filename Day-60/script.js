ac6ad1b7c97d645973647e31366df4886d5f5d2e
reddirani8496 <reddiranibattuluri@gmail.com>
2025-12-05 22:14:51 +0530

    Student CRUD operations

--- Day-60/script.js
+++ Day-60/script.js
@@ -1,18 +1,54 @@
 const fetchStudentDetailsURL = "http://localhost:8080/student/allStudents";
+const addStudentURL = "http://localhost:8080/student/insert";
+const updateStudentDetailsURL = "http://localhost:8080/student/update";
+const deleteStudentDetailsURL = "http://localhost:8080/student/delete/";
 
 const fetchData = async () => {
   const response = await fetch(fetchStudentDetailsURL);
   const data = await response.json();
-  console.log(data);
-
   updateTable(data);
 };
 
 fetchData();
 
+function showForm() {
+  form.classList.remove("disableForm");
+  form.classList.add("enableForm");
+  tableElement.classList.remove("table");
+  tableElement.classList.add("disableTable");
+}
+
+const updateStudentEvent = async (student) => {
+  showForm();
+  // show the data in the form
+  document.getElementById("rollNo").value = student.rollNo;
+  document.getElementById("name").value = student.name;
+  document.getElementById("gender").value = student.gender;
+  document.getElementById("course").value = student.course;
+  document.getElementById("duration").value = student.duration;
+  document.getElementById("fee").value = student.fee;
+  updateStudent = true;
+};
+
+const deleteStudentEvent = async (student, tr) => {
+  let deleteURL = `${deleteStudentDetailsURL}${student.rollNo}`;
+  let response = await fetch(deleteURL, { method: "DELETE" });
+
+  if (response.ok) {
+    alert("Student Deleted successfully");
+    tr.remove();
+  } else {
+    alert("Error in deleting in student");
+  }
+};
+
 let tableElement = document.querySelector("table");
+let tableBody = document.querySelector("tbody");
+let updateStudent = false;
 
 const updateTable = (data) => {
+  console.log("line 50", data);
+  tableBody.innerHTML = "";
   for (let student of data) {
     let tr = document.createElement("tr");
     for (let value in student) {
@@ -21,27 +57,25 @@ const updateTable = (data) => {
       tr.appendChild(td);
     }
 
+    let updateButton = document.createElement("button");
+    updateButton.innerText = "Update";
+    updateButton.classList.add("update");
+    updateButton.addEventListener("click", () => updateStudentEvent(student));
+    let td = document.createElement("td");
+    td.appendChild(updateButton);
+    tr.appendChild(td);
+
     let deleteButton = document.createElement("button");
     deleteButton.innerText = "Delete";
     deleteButton.classList.add("delete");
-
-    deleteButton.addEventListener("click", async () => {
-      let deleteURL = `http://localhost:8080/student/delete/${student.rollNo}`;
-      let response = await fetch(deleteURL, { method: "DELETE" });
-
-      if (response.ok) {
-        alert("Student Deleted successfully");
-        tr.remove();
-      } else {
-        alert("Error in deleting in student");
-      }
-    });
-
-    let td = document.createElement("td");
+    deleteButton.addEventListener("click", () =>
+      deleteStudentEvent(student, tr)
+    );
+    td = document.createElement("td");
     td.appendChild(deleteButton);
     tr.appendChild(td);
 
-    tableElement.appendChild(tr);
+    tableBody.appendChild(tr);
   }
 };
 
@@ -49,13 +83,8 @@ let addStudentbtn = document.querySelector(".addStudent");
 let form = document.querySelector("form");
 
 addStudentbtn.addEventListener("click", () => {
-  form.classList.remove("disableForm");
-  form.classList.add("enableForm");
-  tableElement.remove("table");
-  tableElement.classList.add("disableTable");
+  showForm();
 });
-
-const addStudentURL = "http://localhost:8080/student/insert";
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -66,7 +95,6 @@ form.addEventListener("submit", async (e) => {
   let duration = document.getElementById("duration").value;
   let fee = document.getElementById("fee").value;
 
-  console.log(rollNo, name, gender, course, duration, fee);
   let student = {
     rollNo: Number(rollNo),
     name: name,
@@ -76,23 +104,37 @@ form.addEventListener("submit", async (e) => {
     fee: Number(fee),
   };
 
-  console.log(student);
-
-  let response = await fetch(addStudentURL, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(student),
-  });
+  let response;
+  if (updateStudent) {
+    response = await fetch(updateStudentDetailsURL, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(student),
+    });
+  } else {
+    response = await fetch(addStudentURL, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(student),
+    });
+  }
 
   if (response.ok) {
-    alert("Sudent added successfully");
+    if (updateStudent) {
+      alert("Student updated successfully");
+    } else {
+      alert("Sudent added successfully");
+    }
     form.classList.add("disableForm");
     form.classList.remove("enableForm");
     tableElement.classList.add("table");
     tableElement.classList.remove("disableTable");
     form.reset();
     fetchData();
+    updateStudent = false;
   }
 });
